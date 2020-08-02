54900a8eff6b6279e537302449802576ee55e83b
mnquaser <rafefanam1@gmail.com>
2020-08-03 01:47:26 +0600

    solved prob:Watermelon

---
+++ Codeforces_solve/Watermelon/Watermelon/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://codeforces.com/problemset/problem/4/A
+
+using System;
+
+namespace Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Watermelon = Convert.ToInt32(Console.ReadLine());
+            string result = "";
+            if (Watermelon <= 2)
+                result = "NO";
+            else
+                result = (Watermelon % 2 == 0) ? "YES" : "NO";
+            Console.WriteLine(result);
+         }   
+    }
+}
