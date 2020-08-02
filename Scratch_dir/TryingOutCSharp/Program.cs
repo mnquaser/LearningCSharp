76381aee63986e426cb57b8091335549ceaae197
mnquaser <rafefanam1@gmail.com>
2020-08-02 17:24:07 +0600

    rearranged the repo for multiple projects

---
+++ Scratch_dir/TryingOutCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryingOutCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
