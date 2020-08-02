f721780cc8352ec4b74d43615357d9456a5841ec
mnquaser <rafefanam1@gmail.com>
2020-08-02 14:15:59 +0600

    added program3 code

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -8,17 +8,21 @@ namespace LearningCSharp
     {
         static void Main(string[] args)
         {
-            //program HelloWorld
+            //program1 HelloWorld
             Console.WriteLine("Hello World");
             Console.ReadLine();
 
-            //program Variables
+            //program2 Variables
             int x = 32;
             int y = x + 2;
             Console.WriteLine(y);
             Console.ReadLine();
 
-
+            //program3 Greetings
+            Console.WriteLine("What is your name ?");
+            Console.Write("Type your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + ". How are yo? ");
         }
     }
 }
