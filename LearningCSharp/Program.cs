c8abc4be9da6e27b9587d17501e6438f7798fe77
mnquaser <rafefanam1@gmail.com>
2020-08-02 12:49:43 +0600

    Add project files.

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
