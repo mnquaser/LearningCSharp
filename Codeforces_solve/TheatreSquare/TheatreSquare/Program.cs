c46d7e1de443365336eceaa62e0e765002976abb
mnquaser <rafefanam1@gmail.com>
2020-08-06 02:39:50 +0600

    Solved Codeforces Problems

---
+++ Codeforces_solve/TheatreSquare/TheatreSquare/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TheatreSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var n = decimal.Parse(input[0]);
+            var m = decimal.Parse(input[1]);
+            var a = decimal.Parse(input[2]);
+            var widthStone = Math.Ceiling(m/a);
+            var heightStone = Math.Ceiling(n/a);
+
+
+            Console.WriteLine(heightStone * widthStone);
+        }
+    }
+}
---
+++ Codeforces_solve/WayTooLongWords/WayTooLongWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WayTooLongWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> givenWords = new List<string>();
+            int lineCount = Convert.ToInt32(Console.ReadLine());
+            foreach (var item in Enumerable.Range(0, lineCount))
+            {
+                givenWords.Add((Console.ReadLine()));
+            }
+            foreach (string item in givenWords)
+            {
+                if (item.Length > 10)
+                    Console.WriteLine("{0}{1}{2}", item[0], item.Length - 2, item[item.Length - 1]);
+                else
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
