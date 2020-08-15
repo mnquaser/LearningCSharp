b58f0256a0099522b1ca91ce4c1124c9dee8578f
mnquaser <rafefanam1@gmail.com>
2020-08-15 17:56:32 +0600

    used methods to shorten the code

--- Codeforces_solve/StringTasks/StringTasks/Program.cs
+++ Codeforces_solve/StringTasks/StringTasks/Program.cs
@@ -10,24 +10,37 @@ namespace StringTasks
             StringBuilder input = new StringBuilder(Console.ReadLine().ToLower());
             var vowels = "aeiouy".ToCharArray();
             var consonent = "bcdfghjklmnpqrstuvwxz".ToCharArray();
-            foreach (char item in vowels)
-            {
-                string i =  item.ToString();
-                if (input.ToString().Contains(i))
-                {
-                    input.Replace(i, "");
-                }
-            }
 
-            foreach (char item  in consonent)
-            {
-                string i = item.ToString();
-                if (input.ToString().Contains(i))
-                {
-                    input.Replace(i, string.Format(".{0}", i));
-                }
-            }
+            replaceingChars(input, vowels, "");
+
+            replaceingChars(input, consonent, ".{0}");
+
             Console.WriteLine(input);
         }
+
+      
+        /// <summary>
+        /// This method takes a StringBuilder, a charArray and a string. 
+        /// Then it finds every charecters from charArray in StringBuilder and replaces those charecters with the given string.
+        /// </summary>
+        /// <param name="inputString"> The targeted StringBuilder to modify </param>
+        /// <param name="charArray"> The charArray to make change in inputString </param>
+        /// <param name="changedString"> The new string which will be in place of every instances of found chars from charArray, in inputString </param>
+        /// <returns></returns>
+
+        private static System.Text.StringBuilder replaceingChars(System.Text.StringBuilder inputString, char[] charArray, string changedString)
+        {
+            foreach (char charecter in charArray)
+            {
+                string workingString = charecter.ToString();
+                if (inputString.ToString().Contains(workingString))
+                {
+                    inputString.Replace(workingString, string.Format(changedString, workingString));
+                }
+            }
+            return inputString;
+        }
+
+       
     }
 }
